802ff37c35eaf62944ab8e2978f036d1346515b1
emrehancetin <emrehan.cetin@ozu.edu.tr>
2023-12-27 15:02:19 +0300

    The necessary link for the marker was added in 'index.html'. And, improvement has been made for the marker adding process.

--- src/renderer/src/components/GPSWindow.vue
+++ src/renderer/src/components/GPSWindow.vue
@@ -1,22 +1,27 @@
 <script>
  import mapboxgl from 'mapbox-gl';
-
+ 
 export default {
+  name: "GPSWindow",
   data() {
     return {
       map: null,
-      mapboxAccessToken: "pk.eyJ1IjoiZW1yZWhhbmNldGluIiwiYSI6ImNscTVnZndlNDA2emEyanA3azRsb3luYjcifQ.WxKanmEldiLBF_LtCxIWmA"
+      mapboxAccessToken: "pk.eyJ1IjoiZW1yZWhhbmNldGluIiwiYSI6ImNscW5tcXZrODMwOTQycXJxZWVyYzYwNmoifQ.d_BUVkl22ZIhhK1_Qrt34g"
     };
   },
   mounted() {
     mapboxgl.accessToken = this.mapboxAccessToken; // Set the Mapbox access token
 
     this.map = new mapboxgl.Map({
-      container: this.$refs.map,
-      style: 'mapbox://styles/mapbox/satellite-streets-v12',
-      zoom: 17,
+      //container: this.$refs.map,
+      container:'map',
+      //style: 'mapbox://styles/mapbox/satellite-streets-v12',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      zoom: 18,
+      //zoom:9,
       //center: [29.260582, 41.032640],
-      center:[-110.7921091,38.4051641],
+      //center:[-110.7921091,38.4051641],
+      center:[-110.7916091,38.4063641],
 
 
       //41.032640, 29.260582
@@ -31,9 +36,15 @@ export default {
         'tileSize': 512,
         'maxzoom': 18
       });
-
+    
       this.map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
     });
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([-110.7916091,38.4063641])
+        .addTo(this.map);
+      const marker2= new mapboxgl.Marker()
+        .setLngLat([-110.7915091,38.4063641])
+        .addTo(this.map);
   }
 };
 
@@ -41,13 +52,13 @@ export default {
 </script>
 <template>
   <div class="">
-    <div ref="map" style="width: 100%; height: 500px;"></div>
+    <div id="map" ref="map" style="width:100%;"></div>
   </div>
 </template>
 <style scoped>
 
-.container{
-  border: 1px solid red;
+#map{
+  height: 100vh;
 }
 
 
