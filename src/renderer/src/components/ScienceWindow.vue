8cb946fa577a827747fd2f2c22466fec427ca328
Tanay Alpkonur <107621923+taannaayyy@users.noreply.github.com>
2024-02-17 15:09:31 +0300

    Update ScienceWindow.vue

--- src/renderer/src/components/ScienceWindow.vue
+++ src/renderer/src/components/ScienceWindow.vue
@@ -1,22 +1,68 @@
-<script setup>
-import DeepDirtIcon from "../../../../resources/icons/DeepDirtIcon.svg"
-
-</script>
-
 <script>
+import ArchiveNavigator from './science/ArchiveNavigator.vue';
+
 export default {
-  props:{
-    roverData: {},
+  name: 'ScienceWindow',
+  components: {
+    ArchiveNavigator
+  },
+  data() {
+    return {
+      archives: [
+        { id: 1, name: 'Archive 1' },
+        { id: 2, name: 'Archive 2' },
+        { id: 3, name: 'Archive 3' },
+        { id: 4, name: 'Archive 4' },
+        { id: 5, name: 'Archive 5' }
+      ],
+      selectedArchiveName: ''
+    };
+  },
+  methods: {
+    handleNewLog() {
+      const newArchiveId = this.archives.length + 1;
+      const newArchiveName = `Archive ${newArchiveId}`;
+      const newArchive = { id: newArchiveId, name: newArchiveName };
+      this.archives.push(newArchive);
+    },
+    handleCapturePanaroma() {
+      console.log('Capture Panaroma');
+    },
+    handleCaptureHighDef() {
+      console.log('Capture High-Def');
+    },
+    handleArchiveSelected(archive) {
+      this.selectedArchiveName = archive.name;
+    }
   }
-}
+};
 </script>
 
 <template>
-  <div :class="'MainClass'" v-if = "roverData != undefined ? roverData : 0">
+  <div>
+    <div class="space-between"></div>
+    <ArchiveNavigator :archives="archives" @archive-selected="handleArchiveSelected" />
+
+    <div class="space-between"></div>
+
+    <div class="button-container">
+      <button style="background-color: red; margin-right: 100px; margin-left: 20px" @click="handleNewLog">
+        Create New Log
+      </button>
+      <button style="background-color: red; margin-right: 100px" @click="handleCapturePanaroma">
+        Capture Panaroma
+      </button>
+      <button style="background-color: red" @click="handleCaptureHighDef">Capture High-Def</button>
+    </div>
+
+    <div class="space-between"></div>
     
+    <div v-if="selectedArchiveName">Selected Archive: {{ selectedArchiveName }}</div>
   </div>
 </template>
 
 <style scoped>
-
+.space-between {
+  margin-bottom: 20px;
+}
 </style>
